b397177a8f3351b54f8299cf093b251146b0047b
mayankmandloi <mayankmandloi009@gmail.com>
2017-06-21 21:41:38 +0530

    new file:   dataRandom.html 	new file:   index.html 	new file:   js/angular.js 	new file:   js/angular.min.js 	new file:   js/dataRandon.js 	new file:   js/photos.js 	new file:   js/script.js 	new file:   js/technology.js 	new file:   photos.html 	new file:   tech.html

---
+++ js/technology.js
@@ -0,0 +1,13 @@
+var app = angular.module("technologyModule",[]);
+app.controller("techController",function($scope) {
+  $scope.technologies = [{name:"Java",fee:8000},{name:"Angular",fee:10000},{name:"Web Designing",fee:15000},{name:"C & C++",fee:5000},{name:"PHP",fee:8000}];
+  $scope.addTech = function() {
+    var obj ={};
+    obj.name = $scope.Tname;
+    obj.fee= $scope.Tfee;
+    $scope.technologies.push(obj);
+  }
+
+
+
+})
